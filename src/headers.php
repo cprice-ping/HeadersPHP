583c066d7a70229ba89e4ae587a2ac97aa21d05c
Chris Price <cprice@pingidentity.com>
2019-11-14 14:35:12 -0800

    Update headers.php

--- src/headers.php
+++ src/headers.php
@@ -12,6 +12,8 @@
 		<title>Sample Header App</title>
 		<meta name="description" content="Page to display Headers -- useful when protected by PingAccess" />
                 <meta name="author" content="Ping Identity" />
+
+		<link rel="stylesheet" href="css/headers.css">
 		<!-- Bootstrap -->
 		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 		<link href='http://fonts.googleapis.com/css?family=Nunito:400,300,700' rel='stylesheet' type='text/css'>
